e358667d664b7ae9eb9caec67953fcb31cbf6867
Chinedu Onugu <teddybluez@users.noreply.github.com>
2017-08-22 05:29:12 -0400

    liri-node-app

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'vUVanzJ5z0ZfAi1vVnQ5fSG26',
+  consumer_secret: 'WTuMqpFdffuIf7zI71o5zAWQH89Dp1mAwA5fkbH7fvbiwi4vEx',
+  access_token_key: '899646432549814273-BoHLBDZPmFmHmZ1AA7qyEmVyIhv2L8i',
+  access_token_secret: 'sPcfht0hOA2t2QyIW6ucYMqH0hKT6rCdOzj3nX75T7UiE',
+}
+
